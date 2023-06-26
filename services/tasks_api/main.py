1d435f6da0f67e8d79bb824ef7a21a43cdb2d8ac
Mike Thomas <mikethomas84@gmail.com>
2023-06-26 20:22:37 +0100

    fastapi cors lock

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from typing import Union
 
@@ -14,14 +15,9 @@ from store import TaskStore
 
 app = FastAPI()
 
-origins = [
-    "http://127.0.0.1:5173",
-    "http://development-tasks-api-buck.s3-website-eu-west-1.amazonaws.com",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
